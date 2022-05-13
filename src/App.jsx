c819ee1995e3dd7c9f902d678d28c6bea7078dff
Agustin <agustinsanchezx@gmail.com>
2022-05-12 23:08:08 -0300

    add proyecto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header/>
+
+      <ItemListContainer/>
+
+      <Footer/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function ItemListContainer() {
+
+    const productos = [
+        {id: "1", name: "random", description: "lorem insput"},
+        {id: "2", name: "random2", description: "lorem insput"},
+        {id: "3", name: "random3", description: "lorem insput"},
+        {id: "4", name: "random4", description: "lorem insput"},
+    ]
+
+    const traerProd = new Promise ((resolve, reject) => {
+
+        let condition = true
+        setTimeout(() => {
+            if(condition){
+                resolve(productos)
+            }else{
+                reject("Error 404")
+            }
+        }, 2000)
+    })
+
+    return (
+        <div>ItemListContainer</div>
+    )
+}
